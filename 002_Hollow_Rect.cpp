2b78419f1fdbe4c936b12191d19f228b1719db98
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2023-12-02 00:37:42 +0530

    Some comments added

--- 002_Hollow_Rect.cpp
+++ 002_Hollow_Rect.cpp
@@ -3,11 +3,16 @@ using namespace std;
 int main() {
     int ROWS=10;
     int COLS=7;
+
+    // Outer Loop
     for (int row=0;row<ROWS;row++) {
+        // Inner Loop
         for (int col=0;col<COLS;col++) {
+            // If zeroth or last row, then print star
             if (row==0||row==ROWS-1) {
                 cout << "* ";
             }
+            // Else print 1 star at start and end filled with spaces in b/w
             else {
                 if (col==0||col==COLS-1) {
                     cout << "* ";
