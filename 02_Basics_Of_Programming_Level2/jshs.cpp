c96a7ca6dfc4343d8103afab50c7e5b7571342ee
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-01-24 22:05:52 +0530

    Tries Quiz of Week 2 in this file

---
+++ 02_Basics_Of_Programming_Level2/jshs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// bool some(unsigned int x) {
+//     return (!(x&(x-1)));
+// }
+int main() {
+    // cout << some(0);
+    // int num = 8;
+    // cout << (~num + 1);
+    // if (~0 == 1) cout <<"yes";
+    // else cout << "no";
+    // int x = -5;
+    // x = x >> 1;
+    // cout << x;
+    return 0;
+}
